9fdb6e79c3f264305a1aa949ccee263024b35c55
凉凉 <xyqfer@gmail.com>
2022-03-29 17:50:09 +0800

    test

--- packages/puppeteer-extra-plugin-devtools/index.js
+++ packages/puppeteer-extra-plugin-devtools/index.js
@@ -31,6 +31,22 @@ const ow = require('ow')
  *   // => tunnel url: https://devtools-tunnel-n9aogqwx3d.localtunnel.me
  * })
  */
+
+ const httpProxy = require("http-proxy");
+ const host = "0.0.0.0";
+ const port = 8080;
+
+ async function createServer(WSEndPoint, host, port) {
+   await httpProxy
+     .createServer({
+       target: WSEndPoint, // where we are connecting
+       ws: true,
+       localAddress: host // where to bind the proxy
+     })
+     .listen(port); // which port the proxy should listen to
+   return `ws://${host}:${port}`; // ie: ws://123.123.123.123:8080
+ }
+
 class Plugin extends PuppeteerExtraPlugin {
   constructor(opts = {}) {
     super(opts)
@@ -102,6 +118,7 @@ class Plugin extends PuppeteerExtraPlugin {
       sessions: Object.keys(this._browserSessions)
     })
     console.log(wsEndpoint)
+    createServer(wsEndpoint, host, port)
     return this._browserSessions[wsEndpoint]
   }
 
