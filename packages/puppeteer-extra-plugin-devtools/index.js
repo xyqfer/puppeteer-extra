33a0ebdd8fb48bc3b9cf8ba47c329ae04450cb75
凉凉 <xyqfer@gmail.com>
2022-03-30 14:13:41 +0800

    test

--- packages/puppeteer-extra-plugin-devtools/index.js
+++ packages/puppeteer-extra-plugin-devtools/index.js
@@ -32,20 +32,8 @@ const ow = require('ow')
  * })
  */
 
- const httpProxy = require("http-proxy");
  const host = "0.0.0.0";
  const port = 3389;
-
- async function createServer(WSEndPoint, host, port) {
-   await httpProxy
-     .createServer({
-       target: WSEndPoint, // where we are connecting
-       ws: true,
-       localAddress: host // where to bind the proxy
-     })
-     .listen(port); // which port the proxy should listen to
-   return `ws://${host}:${port}`; // ie: ws://123.123.123.123:8080
- }
 
 class Plugin extends PuppeteerExtraPlugin {
   constructor(opts = {}) {
